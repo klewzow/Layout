3a4d2b60c70ef570866ae654372392610f1b8318
Dmitry <klewzow@gmail.com>
2022-11-19 06:46:22 +0200

    add layout alvio

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function ibg() {
+
+    let ibg = document.querySelectorAll(".ibg");
+    for (var i = 0; i < ibg.length; i++) {
+        if (ibg[i].querySelector('img')) {
+            ibg[i].style.backgroundImage = "url(" + ibg[i].querySelector("img").getAttribute("src") + ")";
+        }
+    }
+}
+ibg();
+
+
+
+var burger = document.querySelector('.burger__menu-button');
+burger.addEventListener('click', function () {
+    document.querySelector('.burger__menu-button').classList.toggle("burger__close");
+    document.querySelector('.burger__page').classList.toggle("_active");
+    document.querySelector('body').classList.toggle("_fixed");
+
+
+    // document.querySelector('body').classList.toggle("lock");
+
+}
+);
+
+
+
+
+
+
+
+
+
+
+
